62bcc8a56b64969c07e91ca207eecc70303a40c2
derpmagician <renzin@gmail.com>
2021-07-19 19:06:47 -0500

    fixing ecuation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,10 +14,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	left_side = binary_tree_height(tree->left);
-	right_side = binary_tree_height(tree->right);
+	left_side = 1 + binary_tree_height(tree->left);
+	right_side = 1 + binary_tree_height(tree->right);
 
 	if (left_side > right_side)
-		return (left_side + 1);
-	return (right_side + 1);
+		return (left_side);
+	return (right_side);
 }
